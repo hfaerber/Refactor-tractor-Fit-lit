edb3d30e2e4b744186ab8462edf5922a0db20695
Naomi Campos <naomicampos@nkcampos.local>
2019-10-21 18:14:17 -0600

    Fix merge issues

--- src/SleepRepo.js
+++ src/SleepRepo.js
@@ -19,42 +19,48 @@ class SleepRepo {
       }
       return arr;
     }, []);
+  }
 
-    let avgSleepQualityPerUser = dataByUser.map(user => [...user].splice(-7 * week, 7)).map(user => user.reduce((totalQuality, day) => {
-      totalQuality += day.sleepQuality;
-      return totalQuality;
-    }, 0)).map(user => Number((user / 7).toFixed(2)));
+  returnUsersWeeklyTotals(week, metric) {
+    let dataByUser = this.organizeByUser();
+    let usersWeeklyTotals = dataByUser.map(user => [...user].splice(-7 * week, 7)).map(user => user.reduce((total, day) => {
+      total += day[metric];
+      return total;
+    }, 0))
+    return usersWeeklyTotals
+  }
 
+// pull in both top functions
+  returnWeeksBestSleepQuality(week, metric) {
+    let weekTotals = this.returnUsersWeeklyTotals(week, metric);
+    let weekAverages = weekTotals.map(user => Number((user / 7).toFixed(2)));
     let goodSleepers = [];
-    avgSleepQualityPerUser.forEach((user, index) => {
+    weekAverages.forEach((user, index) => {
       if (user >= 3) {
         goodSleepers.push(index + 1);
       }
     });
-    return goodSleepers;
+    return goodSleepers
   }
+
+
+
+//pull in both top functions
+  // returnWeeklyLongestSleepers(week) {
+  //   let dataByUser = this.organizeByUser();
+  //   let avgSleepHoursPerUser = dataByUser.map(user => [...user].splice(-7 * week, 7)).map(user => user.reduce((totalHours, day) => {
+  //     totalHours += day.hoursSlept;
+  //     return totalHours;
+  //   }, 0));
+  //   console.log(avgSleepHoursPerUser)
+  //   // return [Math.max(...avgSleepHoursPerUser), avgSleepHoursPerUser.indexOf(Math.max(...avgSleepHoursPerUser)) + 1];
+  // }
+
 
   returnLongestSleepers(date) {
     var dateData = this.sleepData.filter(day => day.date === date);
     var sortedSleepers = [...dateData].sort((a, b) => b.hoursSlept - a.hoursSlept);
     return sortedSleepers.filter(day => day.hoursSlept === sortedSleepers[0].hoursSlept).map(user => user.userID);
-  }
-
-  returnWeeklyLongestSleepers(week) {
-    let dataByUser = this.sleepData.reduce((arr, user) => {
-      if (!arr[user.userID - 1]) {
-        arr[user.userID - 1] = [user];
-      } else {
-        arr[user.userID - 1].push(user);
-      }
-      return arr;
-    }, []);
-
-    let avgSleepHoursPerUser = dataByUser.map(user => [...user].splice(-7 * week, 7)).map(user => user.reduce((totalHours, day) => {
-      totalHours += day.hoursSlept;
-      return totalHours;
-    }, 0));
-    return [Math.max(...avgSleepHoursPerUser), avgSleepHoursPerUser.indexOf(Math.max(...avgSleepHoursPerUser)) + 1];
   }
 }
 
--- test/SleepRepo-test.js
+++ test/SleepRepo-test.js
@@ -6,7 +6,7 @@ import allSleepData from '../data/sleep';
 import SleepRepo from '../src/SleepRepo';
 
 
-describe('SleepRepo', () => {
+describe.only('SleepRepo', () => {
 
   let sleepRepo, fullSleepRepo;
   beforeEach(() => {
@@ -26,16 +26,30 @@ describe('SleepRepo', () => {
     expect(sleepRepo.returnAllSleepQuality()).to.equal(2.9);
   });
 
-  it('should return all users who average a sleep quality greater than 3 for a given week', () => {
-    expect(sleepRepo.returnAboveAverageSleepers(1)).to.eql([3]);
+  it('should return weekly sleep quality averages for user', () => {
+    expect(sleepRepo.returnUsersWeeklyTotals(1, 'sleepQuality').length).to.eql(5);
+  });
+
+  it('should return weekly sleep hour averages for user', () => {
+    expect(sleepRepo.returnUsersWeeklyTotals(1, 'hoursSlept').length).to.eql(5);
   });
 
   it('should return the users that slept the most hours for a given date', () => {
     expect(sleepRepo.returnLongestSleepers("2019/06/25")).to.eql([2]);
   });
 
-  it('should return the users who got the most sleep over the last week', () => {
-    expect(sleepRepo.returnWeeklyLongestSleepers(1)).to.eql([57.3, 2])
+  // it('should return the users who got the most sleep over the last week', () => {
+  //   expect(sleepRepo.returnWeeklyLongestSleepers(1)).to.eql([57.3, 2])
+  // });
+
+  it('should return sleep data organized by user ID', () => {
+    expect(sleepRepo.organizeByUser().length).to.equal(5)
   });
+
+
+  it('should return any users with an average sleep quality over 3', () => {
+    expect(sleepRepo.returnWeeksBestSleepQuality(1, 'sleepQuality')).to.eql([3])
+  })
+
 
 });
