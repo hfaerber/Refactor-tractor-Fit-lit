c4faddd641c8024d289f072b864e747837895c72
Heather Faerber <heatherdfaerber@gmail.com>
2019-10-22 18:43:45 -0600

    Correct minor linter issue

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,12 +1,12 @@
 class Sleep extends Utility {
   constructor(dataSet, userID) {
-    super (dataSet, userID);
+    super(dataSet, userID);
   }
 
   returnWeekOfSleepInfo(week, metric) {
-    return this.returnWeekOfData(week, this.singleUserData).map(day => day[metric]);
+    return this.returnWeekOfData(week, this.singleUserData).map(day =>
+      day[metric]);
   }
 }
-
 
 export default Sleep;
